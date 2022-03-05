e827a3f08a26a27ccd42603d464b0af29610821c
Ahmed-Shawareb <ahmedshawareb567@gmail.com>
2022-03-05 08:03:02 -0800

    feat: second release 👽

---
+++ components/TheHeader.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+.theHeader
+    ul.theHeader-menu
+        li: AppLink(to="/" title="home") home
+        li: AppLink(to="/about" title="about") About
+        li: AppLink(to="#" title="") Services
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss"></style>
---
+++ pages/index.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+div
+  .container.pt-xxl
+    .row.justify-content-center
+      .col-md-6
+        div(style="padding-top:4rem;")
+          MultiSelect(v-model="section" :options="options" @searchChange="searchChange" label="🎃 Add Category:")
+</template>
+<script>
+export default {
+  data() {
+    return {
+      section: [],
+      options: [
+        {
+          value: 1,
+          text: "Food"
+        },
+        {
+          value: 2,
+          text: "Animal"
+        },
+        {
+          value: 3,
+          text: "Weapons"
+        }
+      ]
+    };
+  },
+  computed: {
+    availableLocales() {
+      return this.$i18n.locales.filter(i => i.code !== this.$i18n.locale);
+    }
+  },
+  methods: {
+    searchChange(value) {
+      console.log(value);
+    }
+  }
+};
+</script>
+<style lang="scss"></style>
